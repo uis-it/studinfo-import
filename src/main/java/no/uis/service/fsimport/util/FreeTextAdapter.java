f1ca4dc113cca365fa3f2d70e6990dbedc9301e4
Martin Goldhahn <martin.goldhahn@uis.no>
2012-08-13 13:43:36 +0200

    add FreeText to plain text conversion

--- src/main/java/no/uis/service/fsimport/util/FreeTextAdapter.java
+++ src/main/java/no/uis/service/fsimport/util/FreeTextAdapter.java
@@ -11,32 +11,62 @@ import javax.xml.transform.stream.StreamResult;
 import no.uis.service.studinfo.data.FreeText;
 
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
 public class FreeTextAdapter {
 
   public static String stringValue(FreeText text) throws Exception {
+    return convertFreeText(text, new ElementToHtmlConverter());
+  }
+
+  public static String plainStringValue(FreeText text) throws Exception {
+    return convertFreeText(text, new ElementToPlainConverter());
+  }
+
+  private static String convertFreeText(FreeText text, ElementConverter converter) throws Exception {
+    
+    if (text == null || !text.isSetContent()) {
+      return null;
+    }
     StringBuilder sb = new StringBuilder();
     
-    TransformerFactory transformerFactory = null;
     for(Object item : text.getContent()) {
       if (item instanceof String) {
         sb.append(item.toString());
       } else if (item instanceof Element) {
-        if (transformerFactory == null) {
-          transformerFactory = TransformerFactory.newInstance();
-        }
-        DOMSource domSource = new DOMSource((Node)item);
-        StringWriter sw = new StringWriter();
-        StreamResult streamResult = new StreamResult(sw);
-        Transformer transformer = transformerFactory.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-        transformer.setOutputProperty(OutputKeys.METHOD, "html");
-        transformer.transform(domSource, streamResult);
-        sb.append(sw.toString());
+        sb.append(converter.stringValue((Element)item));
       }
     }
     return sb.toString();
   }
+  
+  private static class ElementToHtmlConverter implements ElementConverter {
 
+    private TransformerFactory factory = TransformerFactory.newInstance();
+    
+    @Override
+    public String stringValue(Element element) throws Exception {
+      DOMSource domSource = new DOMSource(element);
+      StringWriter sw = new StringWriter();
+      StreamResult streamResult = new StreamResult(sw);
+      Transformer transformer = factory.newTransformer();
+      transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+      transformer.setOutputProperty(OutputKeys.METHOD, "html");
+      transformer.transform(domSource, streamResult);
+      return sw.toString();
+    }
+    
+  }
+  
+  public static class ElementToPlainConverter implements ElementConverter {
+
+    @Override
+    public String stringValue(Element element) throws Exception {
+      return element.getTextContent();
+    }
+  }
+
+  private interface ElementConverter {
+    
+    String stringValue(Element element) throws Exception;
+  }
 }
