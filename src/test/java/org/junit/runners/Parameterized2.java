395adcb2758abd39b1c33e8bb1e4e8139ea8ad5d
Martin Goldhahn <martin.goldhahn@uis.no>
2012-10-04 15:48:39 +0200

    update dependency versions

--- src/test/java/org/junit/runners/Parameterized2.java
+++ src/test/java/org/junit/runners/Parameterized2.java
@@ -7,7 +7,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.runner.Runner;
